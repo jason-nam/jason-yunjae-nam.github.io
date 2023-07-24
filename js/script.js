79b88c8a2f469d20ad4372c2c9b213cd96f22def
namy2 <namy2@mcmaster.ca>
2023-07-24 13:39:21 +0900

    made changes to nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
+// const timeContainer = document.getElementById("time-container");
+// const BIRTH_DAY = "12-10-1815";
+// const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+// const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+// setInterval(() => {
+//   const now = new Date();
+//   const difference = Math.floor(
+//     (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+//   );
+
+//   timeContainer.innerText = intlNumberFormatter.format(difference);
+// }, 1000);
 
 
-const timeContainer = document.getElementById("time-container");
-    const BIRTH_DAY = "12-10-1815";
-    const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-    const intlNumberFormatter = new Intl.NumberFormat("en-US");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const navMenu = document.querySelector(".nav-menu");
 
-    setInterval(() => {
-      const now = new Date();
-      const difference = Math.floor(
-        (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-      );
+  menuBtn.addEventListener("click", () => {
+    // Toggle the "active" class on the menu button
+    menuBtn.classList.toggle("active");
 
-      timeContainer.innerText = intlNumberFormatter.format(difference);
-    }, 1000);
+    // Toggle the "active" class on the navigation menu
+    navMenu.classList.toggle("active");
+  });
+
+  // Close the navigation menu when a navigation item is clicked (for better user experience)
+  const navItems = document.querySelectorAll(".navigation-item a");
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      navMenu.classList.remove("active");
+      menuBtn.classList.remove("active");
+    });
+  });
+});
